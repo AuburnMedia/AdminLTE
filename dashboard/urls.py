7209d7186d219cdc0abddd247aecc275e904b05e
AuburnMedia <nicholashoughton2@gmail.com>
2025-08-20 14:46:35 +1000

    Revert "Add URLs for all pages"
    This reverts commit 4adf7c712d5cf77cf6b9b99a9cd1945675b6671d.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('reports/', views.reports_view, name='reports'),
     path('calculator/', views.footprint_calculator_view, name='calculator'),
-    path('detailed/', views.detailed_view, name='detailed'),
-    path('impact/', views.impact_view, name='impact'),
-    path('checkup/', views.checkup_view, name='checkup'),
 ]
